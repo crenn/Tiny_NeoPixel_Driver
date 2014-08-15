7c8118047f4225d3d075cc2cff9fbed1dafc4934
crenn <crenn6977@gmail.com>
2014-08-15 23:29:00 +1000

    Add in a simple demo program
    Flash each colour of the 8 Neopixels then cycle rainbow colours on all
    pixels.

---
+++ Firmware/QuickDemo/main.c
@@ -0,0 +1,285 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define NEOPIX_NUM	8
+#define NEOPIX_BYTE	(NEOPIX_NUM*3)
+
+const uint16_t numBytes = NEOPIX_BYTE;
+volatile uint8_t hi = 0; // PORT w/output bit set high
+volatile uint8_t lo = 0; // PORT w/output bit set low
+uint8_t pixels[NEOPIX_BYTE] = {0x00};
+
+uint8_t colour[3];
+
+void ioinit(void);
+void setupNeopixels(void);
+void writeNeoPixel(uint8_t r, uint8_t g, uint8_t bl);
+void writeNeoPixelByte(void);
+void showLEDs(void);
+void rainbow(void);
+void Wheel(uint8_t WheelPos);
+
+int main (void) {
+	uint8_t i = 0;
+	ioinit();
+	
+	setupNeopixels();
+	
+	showLEDs();
+
+	while (1) {
+		while (1) {
+			if (i != 0) {
+				pixels[i-1] = 0x00;
+			} else {
+				pixels[NEOPIX_BYTE-1] = 0x00;
+			}
+			pixels[i++] = 0xFF;
+			if (i >= NEOPIX_BYTE) {
+				i = 0;
+			}
+			_delay_ms(500);
+			showLEDs();
+			if (i == 0)
+				break;
+		}
+		do {
+			rainbow();
+		} while (++i < 3);
+		i = 0;
+	}
+	
+	return 0;
+}
+
+void ioinit(void) {
+	DDRB = 0x02;
+	_delay_ms(500);
+	PORTB = 0x08;
+}
+
+void setupNeopixels(void) {
+	hi = PORTB | 0x02;
+	lo = PORTB & ~0x02;
+}
+
+void showLEDs(void) {
+	uint8_t currNeo = 0;
+	uint8_t temp = 0;
+	cli();
+	while (currNeo < NEOPIX_BYTE) {
+		colour[0] = pixels[currNeo++];
+		colour[1] = pixels[currNeo++];
+		colour[2] = pixels[currNeo++];
+		writeNeoPixelByte();
+	}
+	sei();
+}
+
+void rainbow(void) {
+  uint16_t i, j;
+
+  for(j=0; j<256; j++) {
+	Wheel(j);
+	for (i=0; i < NEOPIX_NUM; i++) {
+		writeNeoPixelByte();
+	}
+    _delay_ms(50);
+  }
+}
+
+void Wheel(uint8_t WheelPos) {
+  uint8_t temp;
+  if(WheelPos < 85) {
+   temp = WheelPos * 3;
+   colour[0] = 255 - temp;
+   colour[1] = temp;
+   colour[2] = 0;
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   temp = WheelPos * 3;
+   colour[0] = 0;
+   colour[1] = 255 - temp;
+   colour[2] = temp;
+  } else {
+   temp = WheelPos * 3;
+   WheelPos -= 170;
+   colour[0] = temp;
+   colour[1] = 0;
+   colour[2] = 255 - temp;
+  }
+}
+
+void writeNeoPixel(uint8_t r, uint8_t g, uint8_t bl) {
+	colour[0] = g;
+	colour[1] = r;
+	colour[2] = bl;
+	writeNeoPixelByte();
+}
+
+// Assembly is derived from Adafruit's NeoPixel Libraries available here:
+// https://github.com/adafruit/Adafruit_NeoPixel
+void writeNeoPixelByte(void) {
+	volatile uint16_t
+    i   = 3; // Loop counter
+  volatile uint8_t
+   *ptr = colour,   // Pointer to next byte
+    b   = *ptr++;   // Current byte value
+	
+	// volatile uint8_t n1, n2 = 0;
+	
+      // n1 = (b & 0x80) ? hi : lo;
+	  
+
+      // asm volatile(
+       // "headB:"                   "\n\t"
+        // "out  %[port] , %[hi]"    "\n\t"
+        // "mov  %[n2]   , %[lo]"    "\n\t"
+        // "out  %[port] , %[n1]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "sbrc %[byte] , 6"        "\n\t"
+         // "mov %[n2]   , %[hi]"    "\n\t"
+        // "out  %[port] , %[lo]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "out  %[port] , %[hi]"    "\n\t"
+        // "mov  %[n1]   , %[lo]"    "\n\t"
+        // "out  %[port] , %[n2]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "sbrc %[byte] , 5"        "\n\t"
+         // "mov %[n1]   , %[hi]"    "\n\t"
+        // "out  %[port] , %[lo]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "out  %[port] , %[hi]"    "\n\t"
+        // "mov  %[n2]   , %[lo]"    "\n\t"
+        // "out  %[port] , %[n1]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "sbrc %[byte] , 4"        "\n\t"
+         // "mov %[n2]   , %[hi]"    "\n\t"
+        // "out  %[port] , %[lo]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "out  %[port] , %[hi]"    "\n\t"
+        // "mov  %[n1]   , %[lo]"    "\n\t"
+        // "out  %[port] , %[n2]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "sbrc %[byte] , 3"        "\n\t"
+         // "mov %[n1]   , %[hi]"    "\n\t"
+        // "out  %[port] , %[lo]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "out  %[port] , %[hi]"    "\n\t"
+        // "mov  %[n2]   , %[lo]"    "\n\t"
+        // "out  %[port] , %[n1]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "sbrc %[byte] , 2"        "\n\t"
+         // "mov %[n2]   , %[hi]"    "\n\t"
+        // "out  %[port] , %[lo]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "out  %[port] , %[hi]"    "\n\t"
+        // "mov  %[n1]   , %[lo]"    "\n\t"
+        // "out  %[port] , %[n2]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "sbrc %[byte] , 1"        "\n\t"
+         // "mov %[n1]   , %[hi]"    "\n\t"
+        // "out  %[port] , %[lo]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "out  %[port] , %[hi]"    "\n\t"
+        // "mov  %[n2]   , %[lo]"    "\n\t"
+        // "out  %[port] , %[n1]"    "\n\t"
+        // "rjmp .+0"                "\n\t"
+        // "sbrc %[byte] , 0"        "\n\t"
+         // "mov %[n2]   , %[hi]"    "\n\t"
+        // "out  %[port] , %[lo]"    "\n\t"
+        // "sbiw %[count], 1"        "\n\t"
+        // "out  %[port] , %[hi]"    "\n\t"
+        // "mov  %[n1]   , %[lo]"    "\n\t"
+        // "out  %[port] , %[n2]"    "\n\t"
+        // "ld   %[byte] , %a[ptr]+" "\n\t"
+        // "sbrc %[byte] , 7"        "\n\t"
+         // "mov %[n1]   , %[hi]"    "\n\t"
+        // "out  %[port] , %[lo]"    "\n\t"
+        // "brne headB"              "\n"
+      // : [byte] "+r" (b), [n1] "+r" (n1), [n2] "+r" (n2), [count] "+w" (i)
+      // : [port] "I" (_SFR_IO_ADDR(PORTB)), [ptr] "e" (ptr), [hi] "r" (hi),
+        // [lo] "r" (lo));
+		
+		// const volatile uint8_t
+    // *port = _SFR_IO_ADDR(PORTB);         // Output PORT register
+		
+		// volatile uint8_t next, bit;
+
+    // next = lo;
+    // bit  = 8;
+
+    // asm volatile(
+     // "head20:"                   "\n\t" // Clk  Pseudocode    (T =  0)
+	 
+      // "st   %a[port],  %[hi]"    "\n\t" // 2    PORT = hi     (T =  2)
+	  
+      // "sbrc %[byte],  7"         "\n\t" // 1-2  if(b & 128)
+       // "mov  %[next], %[hi]"     "\n\t" // 0-1   next = hi    (T =  4)
+      // "dec  %[bit]"              "\n\t" // 1    bit--         (T =  5)
+      // "st   %a[port],  %[next]"  "\n\t" // 2    PORT = next   (T =  7)
+      // "mov  %[next] ,  %[lo]"    "\n\t" // 1    next = lo     (T =  8)
+      // "breq nextbyte20"          "\n\t" // 1-2  if(bit == 0) (from dec above)
+      // "rol  %[byte]"             "\n\t" // 1    b <<= 1       (T = 10)
+      // "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 12)
+      // "nop"                      "\n\t" // 1    nop           (T = 13)
+      // "st   %a[port],  %[lo]"    "\n\t" // 2    PORT = lo     (T = 15)
+      // "nop"                      "\n\t" // 1    nop           (T = 16)
+      // "rjmp .+0"                 "\n\t" // 2    nop nop       (T = 18)
+      // "rjmp head20"              "\n\t" // 2    -> head20 (next bit out)
+     // "nextbyte20:"               "\n\t" //                    (T = 10)
+      // "ldi  %[bit]  ,  8"        "\n\t" // 1    bit = 8       (T = 11)
+      // "ld   %[byte] ,  %a[ptr]+" "\n\t" // 2    b = *ptr++    (T = 13)
+      // "st   %a[port], %[lo]"     "\n\t" // 2    PORT = lo     (T = 15)
+      // "nop"                      "\n\t" // 1    nop           (T = 16)
+      // "sbiw %[count], 1"         "\n\t" // 2    i--           (T = 18)
+       // "brne head20"             "\n"   // 2    if(i != 0) -> (next byte)
+      // : [port]  "+e" (port),
+        // [byte]  "+r" (b),
+        // [bit]   "+r" (bit),
+        // [next]  "+r" (next),
+        // [count] "+w" (i)
+      // : [ptr]    "e" (ptr),
+        // [hi]     "r" (hi),
+        // [lo]     "r" (lo));
+		
+		volatile uint8_t next, bit;
+
+    next = lo;
+    bit  = 8;
+
+    asm volatile(
+     "head20:"                   "\n\t" // clk  pseudocode    (t =  0)
+      "out  %[port] , %[hi]"     "\n\t" // 1    port = hi     (t =  1)
+      "nop"                      "\n\t" // 1    nop           (t =  2)
+      "sbrc %[byte],  7"         "\n\t" // 1-2  if(b & 128)
+       "mov  %[next], %[hi]"     "\n\t" // 0-1   next = hi    (t =  4)
+      "dec  %[bit]"              "\n\t" // 1    bit--         (t =  5)
+      "out  %[port] , %[next]"   "\n\t" // 1    port = next   (t =  6)
+      "nop"                      "\n\t" // 1    nop           (t =  7)
+      "mov  %[next] ,  %[lo]"    "\n\t" // 1    next = lo     (t =  8)
+      "breq nextbyte20"          "\n\t" // 1-2  if(bit == 0) (from dec above)
+      "rol  %[byte]"             "\n\t" // 1    b <<= 1       (t = 10)
+      "rjmp .+0"                 "\n\t" // 2    nop nop       (t = 12)
+      "nop"                      "\n\t" // 1    nop           (t = 13)
+      "out  %[port] , %[lo]"     "\n\t" // 1    port = lo     (t = 14)
+      "rjmp .+0"                 "\n\t" // 2    nop nop       (t = 16)
+      "rjmp .+0"                 "\n\t" // 2    nop nop       (t = 18)
+      "rjmp head20"              "\n\t" // 2    -> head20 (next bit out)
+     "nextbyte20:"               "\n\t" //                    (t = 10)
+      "ldi  %[bit]  ,  8"        "\n\t" // 1    bit = 8       (t = 11)
+      "ld   %[byte] ,  %a[ptr]+" "\n\t" // 2    b = *ptr++    (t = 13)
+      "out  %[port] , %[lo]"     "\n\t" // 1    port = lo     (t = 14)
+      "rjmp .+0"                 "\n\t" // 2    nop nop       (t = 16)
+      "sbiw %[count], 1"         "\n\t" // 2    i--           (t = 18)
+       "brne head20"             "\n"   // 2    if(i != 0) -> (next byte)
+      : [byte]  "+r" (b),
+        [bit]   "+r" (bit),
+        [next]  "+r" (next),
+        [count] "+w" (i)
+      : [port] "I" (_SFR_IO_ADDR(PORTB)),
+        [ptr]    "e" (ptr),
+        [hi]     "r" (hi),
+        [lo]     "r" (lo));
+}
