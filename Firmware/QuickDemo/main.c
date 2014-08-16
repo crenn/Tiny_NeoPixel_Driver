40224fcaffb44fa0ca244dc5ba4cd86340971a58
crenn <crenn6977@gmail.com>
2014-08-16 19:08:04 +1000

    Add in handling to turn off device
    Debounce the power button, and pull the pin low after a short period.

--- Firmware/QuickDemo/main.c
+++ Firmware/QuickDemo/main.c
@@ -2,23 +2,49 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+//Timer 0 used for UART handling
+
+//Timer 1 used for timing and debouncing
+#define TIM1PEROID 5 //ms
+#define TURNOFFAFTER	(1000/TIM1PEROID) //ms
+#define STAYONAFTER		(500/TIM1PEROID) //ms
+#define MODESWAFTER		(50/TIM1PEROID) //ms
+
 #define NEOPIX_NUM  8
 #define NEOPIX_BYTE  (NEOPIX_NUM*3)
 
 const uint16_t numBytes = NEOPIX_BYTE;
 volatile uint8_t hi = 0; // PORT w/output bit set high
 volatile uint8_t lo = 0; // PORT w/output bit set low
+volatile uint16_t time = TURNOFFAFTER;
+volatile uint8_t state = 0;
 uint8_t pixels[NEOPIX_BYTE] = {0x00};
 
-uint8_t colour[3];
+uint8_t colour[3] = {0x00};
 
 void ioinit(void);
 void setupNeopixels(void);
 void writeNeoPixel(uint8_t r, uint8_t g, uint8_t bl);
 void writeNeoPixelByte(void);
 void showLEDs(void);
+void colourWipe(uint8_t r, uint8_t g, uint8_t bl);
 void rainbow(void);
 void Wheel(uint8_t WheelPos);
+
+ISR(TIMER1_COMPA_vect) {
+	uint8_t temp = PINB;
+	temp &= 0x08;
+	if (temp != 0) {
+		time = TURNOFFAFTER;
+	} else {
+		if (time > 0) {
+			if (--time == 0) {
+				state = 1;
+			}
+		}
+	}
+	TIFR &= ~(0x40);
+}
 
 int main (void) {
   uint8_t i = 0;
@@ -26,7 +52,14 @@ int main (void) {
   
   setupNeopixels();
   
-  showLEDs();
+  do {
+      writeNeoPixelByte();
+  } while (++i < NEOPIX_NUM);
+  i = 0;
+  // for (i = 0; i < NEOPIX_NUM; i++) {
+	// writeNeoPixelByte();
+  // }
+  // i = 0;
 
   while (1) {
     while (1) {
@@ -48,15 +81,42 @@ int main (void) {
       rainbow();
     } while (++i < 3);
     i = 0;
+	do {
+      colourWipe(0xFF,0x00,0x00);
+	  colourWipe(0x00,0xFF,0x00);
+	  colourWipe(0x00,0x00,0xFF);
+    } while (++i < 3);
+    i = 0;
+	
+	if (state == 1)
+		break;
   }
   
+  i = 0;
+  colour[0] = 0;
+  colour[1] = 0;
+  colour[2] = 0;
+  do {
+      writeNeoPixelByte();
+  } while (++i < NEOPIX_NUM);
+  
+  TCCR1 = 0x00;
+  PORTB &= ~(0x08);
+  DDRB |= 0x08;
+
+  while(1); // switch to sleep here
+
   return 0;
 }
 
 void ioinit(void) {
-  DDRB = 0x02;
-  _delay_ms(500);
-  PORTB = 0x08;
+  DDRB = 0x06;
+  _delay_ms(50);
+  PORTB = 0x19;
+  OCR1A = 156;
+  TIMSK = (1 << OCIE1A);
+  TCCR1 = 0x8A;
+  sei();
 }
 
 void setupNeopixels(void) {
@@ -77,14 +137,31 @@ void showLEDs(void) {
   sei();
 }
 
+void colourWipe(uint8_t r, uint8_t g, uint8_t bl) {
+  uint8_t i, j;
+  colour[0] = g;
+  colour[1] = r;
+  colour[2] = bl;
+  for (i = 1; i <= NEOPIX_NUM; i++) {
+    cli();
+    for (j=0; j < i; j++) {
+		writeNeoPixelByte();
+	}
+	sei();
+	_delay_ms(50);
+  }
+}
+
 void rainbow(void) {
   uint16_t i, j;
 
   for(j=0; j<256; j++) {
   Wheel(j);
+  cli();
   for (i=0; i < NEOPIX_NUM; i++) {
     writeNeoPixelByte();
   }
+  sei();
     _delay_ms(50);
   }
 }
